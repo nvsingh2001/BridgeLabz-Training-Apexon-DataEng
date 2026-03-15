a07d27a0557139ba6bfe428b8223ef98106b7c97
nvsingh2001 <nvsingh2001@hotmail.com>
2026-03-15 16:37:20 +0530

    add: practice some filehandling in python

---
+++ FileHandling/Directory/os_module.py
@@ -0,0 +1,16 @@
+import os
+
+# To get the current working directory
+print(os.getcwd())
+
+os.chdir("/home/ares/projects/BridgeLabz-Training-Apexon-DataEng/Directory/")
+
+print(os.getcwd())
+
+# os.mkdir("Making_Directory_Using_Python")
+
+# os.rename("Making_Directory_Using_Python", "Changing_Name_Of_Directory_Using_Python")
+
+# os.rmdir("Changing_Name_Of_Directory_Using_Python")
+
+# print(os.listdir())
