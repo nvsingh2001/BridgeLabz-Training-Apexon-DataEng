aca73ea6366b3563c7e5a16f591ad0ef26dd56b9
nvsingh2001 <nvsingh2001@hotmail.com>
2026-03-16 11:17:35 +0530

    ref: made changes to the file structure

--- FileHandling/Json/json_basics.py
+++ FileHandling/Json/json_basics.py
@@ -7,3 +7,26 @@ json_str = (
 data = json.loads(json_str)
 
 print(data["location"]["city"])
+
+data_str = json.dumps(data)
+
+print(data_str)
+
+with open("person.json", "r") as file:
+    person_json = json.load(file)
+
+print(person_json)
+
+
+person_json["1"]["location"] = {"city": "Japlaiguri", "country": "India"}
+
+
+person_json["3"] = {
+    "name": "Ayush",
+    "age": 23,
+    "location": {"city": "Noida", "country": "India"},
+}
+
+
+with open("person.json", "w") as file:
+    json.dump(person_json, file)
